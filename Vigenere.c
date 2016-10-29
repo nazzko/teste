c5a761f52b131a3d01cab85d5e4fc874e980823b
nazzko <stnaza8@gmail.com>
2016-10-29 19:53:42 +0300

    Clean code:Final version

--- Vigenere.c
+++ Vigenere.c
@@ -8,23 +8,23 @@ int main(int argc, string argv[])
 {
     int i,y,z;
     
-    if (argc !=2)
+    if (argc !=2)           //перевіряє чи правильно вказаний аргумент програми
     {
        	printf ("try again!");
     	return 1;
     }
-    string k = argv[1];
+    string key = argv[1];
     
-    if (k==NULL)
+    if (k==NULL)            //перевірка чи кодове слово написане
     {
         printf ("write something!");
     	return 1;
     }
    
     
-    for (i = 1; i < strlen(k); i++)
+    for (i = 1; i < strlen(key); i++)
     {
-       if (!isalpha(k[i]))
+       if (!isalpha(key[i]))     //перевірка чи всі введені символи є елементами алфавіту
            {
                printf ("try again!");
                return 1;
@@ -32,20 +32,22 @@ int main(int argc, string argv[])
     }
     string text = get_string();
       
-    if (text==NULL)
+    if (text==NULL)       //перевірка чи написаний текст, який потрібно закодувати
            {
                printf ("Print some words");
                return 1;
            }
     for ( y = 0,z = 0; y < strlen(text);y++)
            {
-    	if (isalpha(text[y]))
+    	if (isalpha(text[y])) //перевірка чи всі введені символи є елементами алфавіту
             {
-    		 if (isupper(text[y]))
-    			printf("%c", ((((text[y] - 'A') + ((toupper(k[z++%strlen(k)]))-'A')%26) % 26) + 'A'));
+    		 if (isupper(text[y]))  //перевірка на велику букву
+                 
+    			printf("%c", ((((text[y] - 'A') + ((toupper(key[z++%strlen(key)]))-'A')%26) % 26) + 'A'));
             
-    		if (islower(text[y]))
-    			printf("%c", ((((text[y] - 'a') + ((tolower(k[z++%strlen(k)]))-'a')%26) % 26) + 'a'));
+    		if (islower(text[y]))   //перевірка на малу букву
+                
+    			printf("%c", ((((text[y] - 'a') + ((tolower(key[z++%strlen(key)]))-'a')%26) % 26) + 'a'));
 
            }
             else
@@ -53,6 +55,6 @@ int main(int argc, string argv[])
 
            }
        		 printf("\n");
-    		return 0;
+    		return 0;   
         
 }
