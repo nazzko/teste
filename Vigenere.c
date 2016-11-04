b079a1f4f4fac0b6719564d745a2e59f7f3f77e6
nazzko <stnaza8@gmail.com>
2016-11-04 22:33:52 +0200

    Code after rewriting

--- Vigenere.c
+++ Vigenere.c
@@ -1,13 +1,24 @@
+/*This program shifr text  with a key word */
+
 #include<stdio.h>
 #include<cs50.h>
 #include<string.h>
-#include<stdlib.h>
+
+    if (key==NULL)            //check whether the key word written
+    {
+        printf ("write something!");
+    	return 1;
+    }     
+    for (i = 1; i < strlen(key); i++)
+    {
+    	//check whether all the input characters are symbols of the alphabet
+       if (!isalpha(key[i]))    
+           {#include<stdlib.h>
 #include<ctype.h>
 
 int main(int argc, string argv[])
 {
-    int i,y,z;
-    
+    int i,y,z;    
     if (argc !=2)           //check if argument isn't 2
     {
        	printf ("try again!");
@@ -15,37 +26,30 @@ int main(int argc, string argv[])
     }
     string key = argv[1];
     
-    if (k==NULL)            //check whether the code word written
-    {
-        printf ("write something!");
-    	return 1;
-    }
-   
-    
-    for (i = 1; i < strlen(key); i++)
-    {
-       if (!isalpha(key[i]))     //check whether all the input characters are symbols of the alphabet
-           {
-               printf ("try again!");
+               printf ("Please write only letters in key ");
                return 1;
            }
     }
     string text = get_string();
       
-    if (text==NULL)       //check  if text is written
+    if (text==NULL)       //check  if text is written                          |
            {
                printf ("Print some words");
                return 1;
            }
     for ( y = 0,z = 0; y < strlen(text);y++)
            {
-    	if (isalpha(text[y]))       //check whether all the input characters are symbols of the alphabet
+        //check whether all the input characters are symbols of the alphabet
+    	if (isalpha(text[y]))       
             {
-    		 if (isupper(text[y]))  //checking in large letters
-                 
+			//checking in large letters
+    		 if (isupper(text[y]))  
+    		 
+                 //26 is a number of letters in  alphabet
     			printf("%c", ((((text[y] - 'A') + ((toupper(key[z++%strlen(key)]))-'A')%26) % 26) + 'A'));
-            
-    		if (islower(text[y]))   //checking in small letters
+				 
+            //checking in small letters
+    		if (islower(text[y]))                                                      
                 
     			printf("%c", ((((text[y] - 'a') + ((tolower(key[z++%strlen(key)]))-'a')%26) % 26) + 'a'));
 
@@ -54,7 +58,7 @@ int main(int argc, string argv[])
      		printf("%c", text[y]);
 
            }
-       		 printf("\n");
-    		return 0;   
+    printf("\n");
+    return 0;   
         
 }
